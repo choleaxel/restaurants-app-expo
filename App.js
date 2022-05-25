fcc463134d2b59e5e2d265b1a1d6eaeceef69a83
Nichole Axel <nfrancis22@gmail.com>
2022-05-25 16:15:15 -0400

    added images

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, ScrollView, Image } from 'react-native';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { ImageBackground } from 'react-native';
+
+const image = {uri: 'https://cdn.pixabay.com/photo/2020/03/25/21/05/pizza-4968645_1280.jpg'}
 
 export default function App() {
   const [allRestaurants, setAllRestaurants] = useState()
@@ -22,11 +25,26 @@ export default function App() {
   return (
 
     <View style={styles.container}>
-      {allRestaurants?.map((singleRest) => {
-        return <Text>{singleRest.name}</Text>
-      })}
+      <ImageBackground resizeMode='cover' source={image} style={styles.container}>
+      <ScrollView >
+      {allRestaurants ? (allRestaurants?.map(singleRest => (
+        <>
+         <Text style={styles.restaurantsName} 
+         key={singleRest.id}> {singleRest.name}
+         </Text>
+         <Image 
+         source={{uri: singleRest.image }}
+         style={{ width: '100%', height: 100 }}
+         />
+        </>
+      ))
+      ) : (
+      <ActivityIndicator size='large' color={'#ff7373'}  />
+      )}
+      </ScrollView>
       <Text>Now I'm hungry!!</Text>
       <StatusBar style="auto" />
+    </ImageBackground>
     </View>
   );
 }
@@ -37,5 +55,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
+  restaurantsName: {
+    color: 'brown',
+    fontSize: 28,
+    marginVertical: 80
+  },
+ 
 });
