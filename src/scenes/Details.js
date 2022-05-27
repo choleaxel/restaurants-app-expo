f3036a2bc60f7f33809a18eb34857f0761e3393e
Nichole Axel <nfrancis22@gmail.com>
2022-05-26 22:00:39 -0400

    ratings added

--- src/scenes/Details.js
+++ src/scenes/Details.js
@@ -1,13 +1,25 @@
-import { View, Text, Image } from "react-native"
+import { View, Text, Image, Button } from "react-native"
 import styles from "../styles"
 import { useContext } from "react"
 import { SingleRestContext } from "../../App"
-import { ActivityIndicator } from "react-native-web";
+import { ActivityIndicator } from "react-native";
 
 
 
 export default function Details () {
     const { currentRest } = useContext(SingleRestContext);
+    const handleRating = (newRating) => {
+      fetch('https://my-first-firestore-na.web.app/restaurants/$(currentRest.id)/rating',{
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({rating: newRating}),
+      })
+      .then(res => res.json())
+      .then(data => setCurrentRest(data))
+      .catch(console.error)
+    }
     return (
       <View style={styles.restaurantCard}>
           {!currentRest
@@ -19,11 +31,19 @@ export default function Details () {
             <Text style={styles.restaurantsName}>{currentRest.name}</Text>
             <Text style={styles.cuisine}>{currentRest.cuisine}</Text>
             <Text style={styles.address}>{currentRest.address}</Text>
-            <Text style={[styles.address, { fontWeight: '700'}]}>
-                Rating: {currentRest.rating}
-            </Text>
+            <Text style={[styles.address, {fontWeight: '700'}]}>Rating {currentRest.rating.toFixed(3)}</Text>
+            <Text style={styles.address}>My Rating:</Text>
+            <View style={{flexDirection: 'row', justifyContent: 'space-evenly', 
+                paddingVertical: 24,
+            }}>
+                <Button onPress={() => handleRating(1)} title='⭐️'/>
+                <Button onPress={() => handleRating(2)} title='⭐️'/>
+                <Button onPress={() => handleRating(3)} title='⭐️'/>
+                <Button onPress={() => handleRating(4)} title='⭐️'/>
+                <Button onPress={() => handleRating(5)} title='⭐️'/>
+            </View>
             </>
-        ) }
+        )}
       </View>  
 
 
